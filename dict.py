e1093408ee79ee618a50573b5c415dc6b0a65731
Yunfan <sun-yf16@mails.tsinghua.edu.cn>
2019-12-21 21:32:22 +0800

    word2dict.py

---
+++ dict.py
@@ -0,0 +1,38 @@
+import gc
+import os
+import gensim
+import time
+import json
+
+data_path = "../data"
+BLOCK = 10000
+files = os.listdir(data_path)
+word2tag = {}
+idx = 0
+for file in files:
+    if not file.endswith(".txt"):
+        continue
+    f = open(os.path.join(data_path, file), "r")
+    for line in f:
+        items = line.strip().split()
+        words = []
+        tags = []
+        for item in items:
+            try:
+                word, tag = item.split("/")
+                words.append(word)
+                tags.append(tag)
+            except ValueError:
+                words.append(item[0])
+                tags.append(item[-1])
+
+        idx += 1
+        for i, word in enumerate(words):
+            if word not in word2tag:
+                word2tag[word] = tags[idx]
+        if idx % BLOCK == 0:
+            print ("load %d" % idx)
+            break
+with open("./word2tag.txt", "w") as f:
+    json.dump(word2tag, f)
+print ("total data: %d, %d" % (idx, len(word2tag)))
