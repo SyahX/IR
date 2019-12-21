f8d95316521496d672c15dad3950518f7753dfd9
Yunfan <sun-yf16@mails.tsinghua.edu.cn>
2019-12-21 17:44:57 +0800

    word2vec.py

--- word2vec.py
+++ word2vec.py
@@ -4,7 +4,7 @@ import gensim
 import time
 
 data_path = "../data"
-
+BLOCK = 10000
 files = os.listdir(data_path)
 sentences = []
 idx = 0
@@ -27,15 +27,24 @@ for file in files:
 
         idx += 1
         sentences.append(words)
-        if idx % 100000 == 0:
-            break
-print ("total data: %d" % idx)
+        if idx % BLOCK == 0:
+            print ("")
+print ("total data: %d, %d" % (idx, idx // BLOCK))
+
+print ("--- build model ---")
+ptr_time = time.time()
+model = gensim.models.Word2Vec(size=128, window=5, min_count=5, workers=40, iter=40)
+model.build_vocab(sentences)
+total = time.time() - ptr_time
+print ("Total time: %.4f" % (total / 60.0))
 
 print ("--- train model ---")
 ptr_time = time.time()
-model = gensim.models.Word2Vec(sentences, size=128, window=5, min_count=5, workers=40, iter=40)
-total = time.time() - ptr_time
-print ("Total time: %.4f" % (total / 60.0))
+length = len(sentences)
+for i in range(length // BLOCK):
+    model.train(sentences[i * BLOCK, (i + 1) * BLOCK])
+    total = time.time() - ptr_time
+    print ("Total time[%d]: %.4f" % ((i + 1) * BLOCK, total / 60.0))
 
 print ("--- save model ---")
 ptr_time = time.time()
